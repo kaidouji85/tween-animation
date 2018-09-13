f0cdbbbfebd24485191929b47d35d77d442c5934
竹内佑介 <kaidouji85@gmail.com>
2018-09-13 21:03:29 +0900

    キャンバスに絵が描けるようになった

--- tween/index.js
+++ tween/index.js
@@ -1,3 +1,18 @@
-(function() {
-  console.log('hello');
-})();
+function loadImage(src) {
+  return new Promise(resolve => {
+    const image = new Image();
+    image.src = src;
+    image.onload = () => resolve(image);
+  });
+}
+
+async function main() {
+  const canvas = document.querySelector('.main-canvas') || document.createElement('canvas');
+  const context = canvas.getContext('2d');
+  const image = await loadImage('./image.png');
+  context.drawImage(image, 0, 0);
+}
+
+window.onload = () => {
+  main();
+};
