09de5dc3344ba641ff562ea740764ed8a703b558
竹内佑介 <kaidouji85@gmail.com>
2018-09-15 13:33:42 +0900

    Web Animation APIのサンプルアニメを変更した

--- sample/03-web-animation-api/js/animation.js
+++ sample/03-web-animation-api/js/animation.js
@@ -1,10 +1,24 @@
-export function animation(image) {
-  return image.animate(
+import {waitUntilFinished} from "./wait-until-finished.js";
+
+export async function animation(image) {
+  const rotate = image.animate(
     [
-      {'opacity': 1},
-      {'opacity': 0},
-      {'opacity': 1},
+      {'transform': 'rotate(0deg)'},
+      {'transform': 'rotate(720deg)'},
     ], {
-      duration: 1000
+      duration: 1000,
+    }
+  );
+  await waitUntilFinished(rotate);
+
+  const move = image.animate(
+    [
+      {'transform': 'translateY(0px)'},
+      {'transform': 'translateY(200px)'},
+      {'transform': 'translateY(0px)'},
+    ], {
+      duration: 1000,
+      easing: 'ease-in-out'
     });
+  await waitUntilFinished(move);
 }
--- sample/03-web-animation-api/js/index.js
+++ sample/03-web-animation-api/js/index.js
@@ -1,22 +1,13 @@
 import {animation} from "./animation.js";
-import {waitUntilFinished} from "./wait-until-finished.js";
 
 async function main() {
   const image1 = document.querySelector('.image01') || document.createElement("img");
   const image2 = document.querySelector('.image02') || document.createElement("img");
   const image3 = document.querySelector('.image03') || document.createElement("img");
 
-  const a1 = animation(image1);
-  a1.play();
-  await waitUntilFinished(a1);
-
-  const a2 = animation(image2);
-  a2.play();
-  await waitUntilFinished(a2);
-
-  const a3 = animation(image3);
-  a3.play();
-  await waitUntilFinished(a3);
+  await animation(image1);
+  await animation(image2);
+  await animation(image3);
 }
 
 window.onload = () => {
